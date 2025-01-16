dc832e899b75cfc92d894eb6ce76bc9f428dd051
Nicholas Armstrong <narmstro@warren.k12.in.us>
2025-01-16 08:21:15 -0500

    Updated Block 05 with Assignment Starter

---
+++ Unit 02_Python Strings/Section 01_Basics of Python Strings/Block 05/Assignment Start/main.py
@@ -0,0 +1,13 @@
+# Assignment 1
+string = input("Enter some text: ")
+string = string.upper()
+string = string.replace(" ", "_")
+print(f"The modified string is: {string}")
+print(f"The length of string is {len(string)}")
+
+#Assignment 2
+# input store in a variable called string
+# string = The string converted to either upper or lower
+# string = use replace and replace " " with ""
+# reversedString = string[use appropriate slicing here]
+# print if string == reversedString
---
+++ Unit 02_Python Strings/Section 01_Basics of Python Strings/Block 05/Example Code/main.py
@@ -0,0 +1,5 @@
+string = "Python"
+print(string[0:3])    # Output: Pyt
+print(string[:3])     # Output: Pyt
+print(string[3:])     # Output: hon
+print(string[::-1])   # Output: nohtyP
