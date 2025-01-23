c5e68d08f34a12e5472fe5553b8a9d4e06c296d7
Nicholas Armstrong <narmstro@warren.k12.in.us>
2025-01-23 08:24:34 -0500

    starter code added for Block 07

---
+++ Unit 02_Python Strings and Data Structures/Section 02_Basics of Python Sets/Block 07/AssignmentStart.py
@@ -0,0 +1,22 @@
+#Assignment 1
+def remove_dups(data : list) -> list:
+    unique_set = set(data)
+    return list(unique_set)
+
+my_list = [1, "Armstrong", 5, 5, 6, "Armstrong", 7, 7, 8, 9]
+print(remove_dups(my_list))
+
+#Assignment 2
+set_1 = {1, 2, 3, 4}
+set_2 = {2, 3, 4, 5}
+# TODO: You can do the rest for #2
+
+#Assignment 3
+
+set_3 = {1, 2, 3}
+element_to_find = 4
+for element in set_3:
+    #TODO: check if element is equal to element_to_find.
+    #TODO: print element_to_find is in the set
+    #TODO: break out of the for loop
+    pass #TODO: remove this pass when done.
---
+++ Unit 02_Python Strings and Data Structures/Section 02_Basics of Python Sets/Block 07/ExampleCode.py
@@ -0,0 +1,13 @@
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+union_set = set1 | set2  # or set1.union(set2)
+print(union_set)  # Output: {1, 2, 3, 4, 5}
+
+intersection_set = set1 & set2  # or set1.intersection(set2)
+print(intersection_set)  # Output: {3}
+
+difference_set = set1 - set2  # or set1.difference(set2)
+print(difference_set)  # Output: {1, 2}
+
+sym_diff_set = set1 ^ set2  # or set1.symmetric_difference(set2)
+print(sym_diff_set)  # Output: {1, 2, 4, 5}
