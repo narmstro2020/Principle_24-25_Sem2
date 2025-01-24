afd05cfe198594d95d048b28c0b25c6aba1b1abc
Nicholas Armstrong <narmstro@warren.k12.in.us>
2025-01-24 09:55:46 -0500

    starter code added for Block 07

--- Unit 02_Python Strings and Data Structures/Section 02_Basics of Python Sets/Block 07/AssignmentStart.py
+++ Unit 02_Python Strings and Data Structures/Section 02_Basics of Python Sets/Block 07/AssignmentStart.py
@@ -1,27 +1,33 @@
-# Assignment 1
-def remove_dups(my_list):
-    my_set = set(my_list)
-    return list(my_set)
+#Assignment 1
+def remove_dups(the_list):
+    unique_set = set(the_list)
+    return list(unique_set)
 
-a_list = [1, 2, 2, 3, 4, 5, 5]
-print(remove_dups(a_list))
+my_list = [1, 2, 3, 3, 4, 5, 5]
+print(remove_dups(my_list))
 
-# Assignment 2
+#Assignment 2
 set1 = {1, 2, 3}
-set2 = {2, 3, 4}
-#TODO: You can do the rest
+set2 = {3, 4, 5}
+print(set1 | set2)
 
-# Assignment 3
+
+#Assignment 3
 set3 = {1, 2, 3, 4, 5}
-element_to_find = 3
+element_to_find = 6
 element_found = False
 for element in set3:
-    #TODO: check if element_to_find is equal to element
-    #TODO: if so set element_found to True
-    #TODO: break out of the for loop
+    #TODO: Check if element is equal to element to find
+    #TODO: set element_found to True
+    #TODO: break out of the loop
     pass #TODO: remove this pass when done.
 
 if element_found:
-    print(f"Element {element_to_find} found in the set")
+    print(f"The element {element_to_find} was found")
 else:
-    print(f"Element {element_to_find} not found in the set")
+    print(f"The element {element_to_find} was not found")
+
+
+
+
+
--- Unit 02_Python Strings and Data Structures/Section 02_Basics of Python Sets/Block 07/ExampleCode.py
+++ Unit 02_Python Strings and Data Structures/Section 02_Basics of Python Sets/Block 07/ExampleCode.py
@@ -7,8 +7,8 @@ intersection_set = set1 & set2  # or set1.intersection(set2)
 print(intersection_set)  # Output: {3}
 
 difference_set = set1 - set2  # or set1.difference(set2)
+print(difference_set)  # Output: {1, 2}difference_set = set1 - set2  # or set1.difference(set2)
 print(difference_set)  # Output: {1, 2}
 
 sym_diff_set = set1 ^ set2  # or set1.symmetric_difference(set2)
 print(sym_diff_set)  # Output: {1, 2, 4, 5}
-
