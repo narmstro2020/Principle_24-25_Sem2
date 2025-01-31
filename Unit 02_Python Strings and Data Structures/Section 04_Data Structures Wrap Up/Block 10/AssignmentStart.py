7744b65af2b02469fa46c07c982ea8d754f9dcbb
Nicholas Armstrong <narmstro@warren.k12.in.us>
2025-01-31 08:26:00 -0500

    Block 10

---
+++ Unit 02_Python Strings and Data Structures/Section 04_Data Structures Wrap Up/Block 10/AssignmentStart.py
@@ -0,0 +1,6 @@
+#Assignment 1
+ten_squares = [] # TODO: put in the square brackets a list comprehension.  See notes
+print(ten_squares)
+
+#Assignment 2
+#TODO:  Total RECALL from previous notes/assignments/etc.
