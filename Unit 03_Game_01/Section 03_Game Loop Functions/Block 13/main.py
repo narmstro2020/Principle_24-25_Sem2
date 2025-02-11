14d02fdabd021a5129e7699cbf4764410ead1d22
Nicholas Armstrong <narmstro@warren.k12.in.us>
2025-02-11 09:24:17 -0500

    Block 13

--- Unit 03_Game_01/Section 03_Game Loop Functions/Block 13/main.py
+++ Unit 03_Game_01/Section 03_Game Loop Functions/Block 13/main.py
@@ -55,6 +55,7 @@ def prep_text(text: str, background_color: tuple[int, int, int], **locations):
     # TODO: return (text_to_return, rect)
     pass  # TODO: remove this when done.
 
+
 # Set Text Blocks
 # TODO: (2025-02-06): assign to (points_text, points_rect)
 # TODO: (continued): the result of the call to prep_text() given f"Burger Points: {burger_points}", ORANGE,
@@ -106,85 +107,93 @@ player_rect.bottom = WINDOW_HEIGHT
 # TODO: (2025-02-06): create a burger_rect variable and aassign from burger_image.get_rect()
 burger_rect.topleft = (random.randint(0, WINDOW_WIDTH - 32), -BUFFER_DISTANCE)
 
-#NOTES:  running and not pause, running and pause, not running
+# NOTES:  running and not pause, running and pause, not running
 
 
 pygame.mixer.music.play()
 running = True
 is_paused = False
 
+
 def check_quit():
-    #TODO: (2025-02-10):  make a for loop:  for event in pygame.event.get():
+    # TODO: (2025-02-10):  make a for loop:  for event in pygame.event.get():
     # TODO: (2025-02-10): start of for loop block
-    # TODO: (2025-02-10):  check if event.type is equal to pygame.QUIT()
-    #TODO: (2025-02-10):  set running to False
+    # TODO: (2025-02-10):  check if event.type is equal to pygame.QUIT
+    # TODO: (2025-02-10):  set running to False
+    # TODO: (2025-02-10):  break
     # TODO: (2025-02-10):  end of for loop block
-    pass #TODO: (2025-02-10):  remove this when done.
+    pass  # TODO: (2025-02-10):  remove this when done.
+
 
 def move_player():
+    global player_image
     keys = pygame.key.get_pressed()
-    #TODO: (2025-02-10): check if keys[pygame.K_LEFT] and player_rect.left > 0:
-    #TODO: (cont.): the if statement block
-    #TODO: (cont.):  the if statement block should subtract player_velocity from player_rect.x
-    #TODO: (cont.):  set the player_image to player_image_left
-    #TODO: (cont.):  end of the if block
+    # TODO: (2025-02-10): check if keys[pygame.K_LEFT] and player_rect.left > 0:
+    # TODO: (cont.): the if statement block
+    # TODO: (cont.):  the if statement block should subtract player_velocity from player_rect.x
+    player_image = player_image_left
+    # TODO: (cont.):  end of the if block
 
-    #TODO: (2025-02-10): check if keys[pygame.K_RIGHT] and player.rect.right < WINDOW_WIDTH:
-    #TODO: (cont.):  the if statement block
-    #TODO: (cont.):  the if statement block should add player_velocity to player_rect.x
-    #TODO: (cont.):  set the player_image to player_image_right
-    #TODO: (cont.):  end of the if block
+    # TODO: (2025-02-10): check if keys[pygame.K_RIGHT] and player.rect.right < WINDOW_WIDTH:
+    # TODO: (cont.):  the if statement block
+    # TODO: (cont.):  the if statement block should add player_velocity to player_rect.x
+    # TODO: (cont.):  set the player_image to player_image_right
+    # TODO: (cont.):  end of the if block
 
-    #TODO: (2025-02-10): check if keys[pygame.K_UP] and player.rect.top > 100
-    #TODO: (cont.): the if statement block
-    #TODO: (cont.):  the if statement block should subtract player_velocity from player_rect.y
-    #TODO: (cont.):  end of the if block
+    # TODO: (2025-02-10): check if keys[pygame.K_UP] and player.rect.top > 100
+    # TODO: (cont.): the if statement block
+    # TODO: (cont.):  the if statement block should subtract player_velocity from player_rect.y
+    # TODO: (cont.):  end of the if block
 
-    #TODO: (2025-02-10): check if keys[pygame.K_DOWN] and player.rect.bottom < WINDOW_HEIGHT
-    #TODO: (cont.): the if statement block
-    #TODO: (cont.):  the if statement block should add player_velocity to player_rect.y
-    #TODO: (cont.):  end of the if block
+    # TODO: (2025-02-10): check if keys[pygame.K_DOWN] and player.rect.bottom < WINDOW_HEIGHT
+    # TODO: (cont.): the if statement block
+    # TODO: (cont.):  the if statement block should add player_velocity to player_rect.y
+    # TODO: (cont.):  end of the if block
 
     engage_boost(keys)
 
-    pass #TODO: (2025-02-10):  remove this when done.
+    pass  # TODO: (2025-02-10):  remove this when done.
+
 
 def engage_boost(keys):
-    #TODO: (2025-02-10): check if keys[pygame.K_SPACE] and boost_level > 0
-    #TODO: (2025-02-10): subtract 1 from boost_level
-    #TODO: (2025-02-10): else set player_velocity to PLAYER_NORMAL_VELOCITY
-    pass #TODO: (2025-02-10):  remove this when done.
+    # TODO: (2025-02-10): check if keys[pygame.K_SPACE] and boost_level > 0
+    # TODO: (2025-02-10): subtract 1 from boost_level
+    # TODO: (2025-02-10): else set player_velocity to PLAYER_NORMAL_VELOCITY
+    pass  # TODO: (2025-02-10):  remove this when done.
+
 
 def move_burger():
-    #TODO: (2025-02-10): add burger_velocity to burger_rect.y
-    burger_points = int(burger_velocity*(WINDOW_HEIGHT - burger_rect.y + 100))
-    pass #TODO: (2025-02-10):  remove this when done.
+    # TODO: (2025-02-10): add burger_velocity to burger_rect.y
+    burger_points = int(burger_velocity * (WINDOW_HEIGHT - burger_rect.y + 100))
+    pass  # TODO: (2025-02-10):  remove this when done.
 
 
 def handle_miss():
-    #TODO: (2025-02-10): if burger_rect.y is greater than WINDOW_HEIGHT:
-    #TODO: (2025-02-10):  the rest of this functions code is in an if statement block
-    #TODO: (2025-02-10):  subtract 1 from player_lives
-    #TODO: (2025-02-10):  call miss_sound's play method
+    # TODO: (2025-02-10): if burger_rect.y is greater than WINDOW_HEIGHT:
+    # TODO: (2025-02-10):  the rest of this functions code is in an if statement block
+    # TODO: (2025-02-10):  subtract 1 from player_lives
+    # TODO: (2025-02-10):  call miss_sound's play method
     burger_rect.topleft = (random.randint(0, WINDOW_WIDTH - 32), -BUFFER_DISTANCE)
-    #TODO: (2025-02-10):  set burger_velocity to STARTING_BURGER_VELOCITY
-    #TODO: (2025-02-10):  set player_rect.centerx to WINDOW_WIDTH // 2
-    #TODO: (2025-02-10):  set player_rect.bottom to WINDOW_HEIGHT
-    #TODO: (2025-02-10):  set boost_level to STARTING_BOOST_LEVEL
-    pass #TODO: (2025-02-10):  remove this when done.
+    # TODO: (2025-02-10):  set burger_velocity to STARTING_BURGER_VELOCITY
+    # TODO: (2025-02-10):  set player_rect.centerx to WINDOW_WIDTH // 2
+    # TODO: (2025-02-10):  set player_rect.bottom to WINDOW_HEIGHT
+    # TODO: (2025-02-10):  set boost_level to STARTING_BOOST_LEVEL
+    pass  # TODO: (2025-02-10):  remove this when done.
+
 
 def check_collisions():
     if player_rect.colliderect(burger_rect):
-        #TODO: (2025-02-10):  add burger_points to score
-        #TODO: (2025-02-10):  add 1 to burgers_eaten
-        #TODO: (2025-02-10):  call bark_sounds' play method
+        # TODO: (2025-02-10):  add burger_points to score
+        # TODO: (2025-02-10):  add 1 to burgers_eaten
+        # TODO: (2025-02-10):  call bark_sounds' play method
         burger_rect.topleft = (random.randint(0, WINDOW_WIDTH - 32), -BUFFER_DISTANCE)
-        #TODO: (2025-02-10):  add BURGER_ACCELERATION to burger_velocity
+        # TODO: (2025-02-10):  add BURGER_ACCELERATION to burger_velocity
 
-        #TODO: (2025-02-10):  add 25 to boost_level
-        #TODO: (2025-02-10):  finally check if boost_level > STARTING_BOOST_LEVEL
-        #TODO: (2025-02-10):  then set boost_level to STARTING_BOOST_LEVEL
-        pass #TODO: (2025-02-10):  remove this when done.
+        # TODO: (2025-02-10):  add 25 to boost_level
+        # TODO: (2025-02-10):  finally check if boost_level > STARTING_BOOST_LEVEL
+        # TODO: (2025-02-10):  then set boost_level to STARTING_BOOST_LEVEL
+        pass  # TODO: (2025-02-10):  remove this when done.
+
 
 def update_hud():
     points_text = font.render("Burger Points: " + str(burger_points), True, ORANGE)
@@ -192,39 +201,28 @@ def update_hud():
     eaten_text = font.render("Burgers Eaten: " + str(burgers_eaten), True, ORANGE)
     lives_text = font.render("Lives: " + str(player_lives), True, ORANGE)
     boost_text = font.render("Boost: " + str(boost_level), True, ORANGE)
-    pass #TODO: (2025-02-10):  remove this when done.
+    pass  # TODO: (2025-02-10):  remove this when done.
+
 
 def check_game_over():
-    #TODO: hold till 2025-02-12
-    pass #TODO: (2025-02-10):  remove this when done.
+    # TODO: hold till 2025-02-12
+    pass  # TODO: (2025-02-10):  remove this when done.
 
 
 def display_hud():
     display_surface.fill(BLACK)
     display_surface.blit(points_text, points_rect)
-    #TODO (2025-02-10): We just blit points_text and points_rect
-    #TODO (cont.):  repeat for score, title, eaten, lives, boost
+    # TODO (2025-02-10): We just blit points_text and points_rect
+    # TODO (cont.):  repeat for score, title, eaten, lives, boost
     pygame.draw.line(display_surface, WHITE, (0, 100), (WINDOW_WIDTH, 100), 3)
-    #TODO (2025-02-10): blit player_image, player_rect
-    #TODO (2025-02-10): blit burger_image, burger_rect
-    pass #TODO: (2025-02-10):  remove this when done.
+    # TODO (2025-02-10): blit player_image, player_rect
+    # TODO (2025-02-10): blit burger_image, burger_rect
+    pass  # TODO: (2025-02-10):  remove this when done.
+
 
 def handle_clock():
     pygame.display.update()
     clock.tick(FPS)
-    pass #TODO: (2025-02-10):  remove this when done.
+    pass  # TODO: (2025-02-10):  remove this when done.
 
 ## GAME LOOP COMING SOON.
-
-
-
-
-
-
-
-
-
-
-
-
-
