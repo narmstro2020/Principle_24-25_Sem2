729456e649273cd55a9d97cf353120962be6f165
Nicholas Armstrong <narmstro@warren.k12.in.us>
2025-02-14 06:55:13 -0500

    Block 15

---
+++ Unit 03_Game_01/Section 04_Game Loop and Conclusion/Block 15/Agenda.md
@@ -0,0 +1,28 @@
+
+# Block 15 Agenda
+- Black Day: Fri, 2/14/2025
+- Gold Day: Wed, 2/19/2025
+
+## Welcome
+
+- Brief Recap on Pygame
+- Open PyCharm
+
+## Aim
+
+- Finalize Game and Turn in 
+
+## Review
+
+- Brief Recap on Pygame
+
+## Relevant Instruction
+
+- See Resources in BurgerDog Folder
+ 
+
+## Interactive, Ownership, Resonate
+- when done and working.  Turn in to Canvas.  
+- Assignment name is: Project 01_Burger Dog
+
+
---
+++ Unit 03_Game_01/Section 04_Game Loop and Conclusion/Block 15/main.py
@@ -0,0 +1,251 @@
+import random, pygame
+
+pygame.init()
+
+# TODO: we need some constants.  WINDOW_WIDTH and WINDOW_HEIGHT, 800, 600
+# TODO: create display_surface assign to it pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+# TODO: call pygame.display.set_caption()  and pass in the argument "Burger Dog"
+
+# TODO: create an FPS variable and assign 60 to it.
+# TODO: create a clock variable and assign pygame.time.Clock()
+
+# TODO: we need the following constants
+# TODO: PLAYER_STARTING_LIVES, PLAYER_NORMAL_VELOCITY, PLAYER_BOOST_VELOCITY, STARTING_BOOST_LEVEL
+# TODO: STARTING_BURGER_VELOCITY, BURGER_ACCELERATION, BUFFER_DISTANCE
+# TODO: values of these variables are: 3, 5, 10, 100, 3, 0.5, 100
+
+score = 0
+burger_points = 0
+burgers_eaten = 0
+
+player_lives = PLAYER_STARTING_LIVES
+# TODO: create a player_velocity variable and assign PLAYER_NORMAL_VELOCITY to it
+# TODO: create a boost_level variable and assign STARTING_BOOST_LEVEL to it
+# TODO: create a burger_velocity variable and assign STARTING_BURGER_VELOCITY to it
+
+# TODO: 3 colors, ORANGE, BLACK, AND WHITE.  BLACK and WHITE are standard RGB, ORANGE is a tuple (246, 170, 54)
+# TODO: please note the colors are tuples.
+
+# TODO: create a font variable and assign pygame.font.Font() passing in "WashYourHand.ttf", 32
+
+# NOTES:  text is a str, background_color is a tuple[int, int, int]
+# NOTES:  **locations is basically a dictionary of str, tuple[int, int] or int
+# NOTES:  this prep_text returns a tuple containing a Font object and a Rectangle object.
+def prep_text(text: str, background_color: tuple[int, int, int], **locations):
+    # TODO: create a text_to_return variable and assign font.render(text, True, background_color)
+    # TODO: create a rect variable and assign text_to_return.get_rect()
+    # TODO: create a for location in locations loop
+    # TODO: for loop block start
+    if location == "topleft":
+        rect.topleft = locations["topleft"]
+    elif location == "centerx":
+        rect.centerx = locations["centerx"]
+    # TODO: (2025-02-06): add this elif portion
+    elif location == "y":
+        rect.y = locations["y"]
+    # TODO: (2025-02-06): add this elif portion
+    elif location == "topright":
+        rect.topright = locations["topright"]
+    # TODO: (2025-02-06): add this elif portion
+    elif location == "center":
+        rect.center = locations["center"]
+    # NOTE:  We'll add more later.
+    # TODO: for loop block end
+    # TODO: return (text_to_return, rect)
+    pass  # TODO: remove this when done.
+
+
+# Set Text Blocks
+# TODO: (2025-02-06): assign to (points_text, points_rect)
+# TODO: (continued): the result of the call to prep_text() given f"Burger Points: {burger_points}", ORANGE,
+# TODO: (continued): topleft=(10, 10)
+
+# TODO: (2025-02-06): assign to (score_text, score_rect)
+# TODO: (continued): the result of the call to prep_text() given f"Score: {score}", ORANGE,
+# TODO: (continued): topleft=(10, 50)
+
+# TODO: (2025-02-06): assign to (title_text, title_rect)
+# TODO: (continued): the result of the call to prep_text() given "Burger Dog", ORANGE,
+# TODO: (continued): centerx=WINDOW_WIDTH // 2, y=10
+
+# TODO: (2025-02-06): assign to (eaten_text, eaten_rect)
+# TODO: (continued): the result of the call to prep_text() given f"Burgers Eaten: {burgers_eaten}", ORANGE,
+# TODO: (continued): centerx=WINDOW_WIDTH // 2, y=50
+
+# TODO: (2025-02-06): assign to (lives_text, lives_rect)
+# TODO: (continued): the result of the call to prep_text() given f"Lives: {player_lives}", ORANGE,
+# TODO: (continued): topright=(WINDOW_WIDTH - 10, 10)
+
+# TODO: (2025-02-06): assign to (boost_text, boost_rect)
+# TODO: (continued): the result of the call to prep_text() given f"Boost: (boost_level)", ORANGE,
+# TODO: (continued): topright=(WINDOW_WIDTH - 10, 50)
+
+# TODO: (2025-02-06): assign to (game_over_text, game_over_rect)
+# TODO: (continued): the result of the call to prep_text() given f"FINAL SCORE: {score}", ORANGE,
+# TODO: (continued): center=(WINDOW_WIDTH // 2, WINDOW_HEIGHT //2)
+
+# TODO: (2025-02-06): assign to (continue_text, continue_rect)
+# TODO: (continued): the result of the call to prep_text() given "Press any key to play again", ORANGE,
+# TODO: (continued): center=(WINDOW_WIDTH // 2, WINDOW_HEIGHT //2 + 64)
+
+# Set sounds and music
+# TODO: (2025-02-06): create a bark_sound variable and assign pygame.mixer.Sound() passing in "bark_sound.wav"
+# TODO: (2025-02-06): create a miss_sound variable and assign pygame.mixer.Sound() passing in "miss_sound.wav"
+# TODO: (2025-02-06): call pygame.mixer.music.load() passing in "bd_background_music.wav"
+
+# Set images
+# TODO: (2025-02-06): create a player_image_right variable and assign pygame.image.load() passing in "dog_right.png"
+# TODO: (2025-02-06): create a player_image_left variable and assign pygame.image.load() passing in "dog_left.png"
+
+player_image = player_image_left
+player_rect = player_image.get_rect()
+player_rect.centerx = WINDOW_WIDTH // 2
+player_rect.bottom = WINDOW_HEIGHT
+
+# TODO: (2025-02-06): create a burger_image variable and assign pygame.image.load() passing in "burger.png"
+# TODO: (2025-02-06): create a burger_rect variable and aassign from burger_image.get_rect()
+burger_rect.topleft = (random.randint(0, WINDOW_WIDTH - 32), -BUFFER_DISTANCE)
+
+# NOTES:  running and not pause, running and pause, not running
+
+
+pygame.mixer.music.play()
+running = True
+is_paused = False
+
+def check_quit():
+    # TODO: (2025-02-10):  make a for loop:  for event in pygame.event.get():
+    # TODO: (2025-02-10): start of for loop block
+    # TODO: (2025-02-10):  check if event.type is equal to pygame.QUIT
+    # TODO: (2025-02-10):  set running to False
+    # TODO: (2025-02-10):  break
+    # TODO: (2025-02-10):  end of for loop block
+    pass  # TODO: (2025-02-10):  remove this when done.
+
+def move_player():
+    global player_image
+    keys = pygame.key.get_pressed()
+    # TODO: (2025-02-10): check if keys[pygame.K_LEFT] and player_rect.left > 0:
+    # TODO: (cont.): the if statement block
+    # TODO: (cont.):  the if statement block should subtract player_velocity from player_rect.x
+    player_image = player_image_left
+    # TODO: (cont.):  end of the if block
+
+    # TODO: (2025-02-10): check if keys[pygame.K_RIGHT] and player.rect.right < WINDOW_WIDTH:
+    # TODO: (cont.):  the if statement block
+    # TODO: (cont.):  the if statement block should add player_velocity to player_rect.x
+    # TODO: (cont.):  set the player_image to player_image_right
+    # TODO: (cont.):  end of the if block
+
+    # TODO: (2025-02-10): check if keys[pygame.K_UP] and player.rect.top > 100
+    # TODO: (cont.): the if statement block
+    # TODO: (cont.):  the if statement block should subtract player_velocity from player_rect.y
+    # TODO: (cont.):  end of the if block
+
+    # TODO: (2025-02-10): check if keys[pygame.K_DOWN] and player.rect.bottom < WINDOW_HEIGHT
+    # TODO: (cont.): the if statement block
+    # TODO: (cont.):  the if statement block should add player_velocity to player_rect.y
+    # TODO: (cont.):  end of the if block
+
+    engage_boost(keys)
+
+    pass  # TODO: (2025-02-10):  remove this when done.
+
+def engage_boost(keys):
+    # TODO: (2025-02-10): check if keys[pygame.K_SPACE] and boost_level > 0
+    # TODO: (2025-02-13): player_velocity = PLAYER_BOOST_VELOCITY
+    # TODO: (2025-02-10): subtract 1 from boost_level
+    # TODO: (2025-02-10): else set player_velocity to PLAYER_NORMAL_VELOCITY
+    pass  # TODO: (2025-02-10):  remove this when done.
+
+def move_burger():
+    # TODO: (2025-02-10): add burger_velocity to burger_rect.y
+    burger_points = int(burger_velocity * (WINDOW_HEIGHT - burger_rect.y + 100))
+    pass  # TODO: (2025-02-10):  remove this when done.
+
+def handle_miss():
+    # TODO: (2025-02-10): if burger_rect.y is greater than WINDOW_HEIGHT:
+    # TODO: (2025-02-10):  the rest of this functions code is in an if statement block
+    # TODO: (2025-02-10):  subtract 1 from player_lives
+    # TODO: (2025-02-10):  call miss_sound's play method
+    burger_rect.topleft = (random.randint(0, WINDOW_WIDTH - 32), -BUFFER_DISTANCE)
+    # TODO: (2025-02-10):  set burger_velocity to STARTING_BURGER_VELOCITY
+    # TODO: (2025-02-10):  set player_rect.centerx to WINDOW_WIDTH // 2
+    # TODO: (2025-02-10):  set player_rect.bottom to WINDOW_HEIGHT
+    # TODO: (2025-02-10):  set boost_level to STARTING_BOOST_LEVEL
+    pass  # TODO: (2025-02-10):  remove this when done.
+
+def check_collisions():
+    if player_rect.colliderect(burger_rect):
+        # TODO: (2025-02-10):  add burger_points to score
+        # TODO: (2025-02-10):  add 1 to burgers_eaten
+        # TODO: (2025-02-10):  call bark_sounds' play method
+        burger_rect.topleft = (random.randint(0, WINDOW_WIDTH - 32), -BUFFER_DISTANCE)
+        # TODO: (2025-02-10):  add BURGER_ACCELERATION to burger_velocity
+
+        # TODO: (2025-02-10):  add 25 to boost_level
+        # TODO: (2025-02-10):  finally check if boost_level > STARTING_BOOST_LEVEL
+        # TODO: (2025-02-10):  then set boost_level to STARTING_BOOST_LEVEL
+        pass  # TODO: (2025-02-10):  remove this when done.
+
+def update_hud():
+    points_text = font.render("Burger Points: " + str(burger_points), True, ORANGE)
+    score_text = font.render("Score: " + str(score), True, ORANGE)
+    eaten_text = font.render("Burgers Eaten: " + str(burgers_eaten), True, ORANGE)
+    lives_text = font.render("Lives: " + str(player_lives), True, ORANGE)
+    boost_text = font.render("Boost: " + str(boost_level), True, ORANGE)
+    pass  # TODO: (2025-02-10):  remove this when done.
+
+def check_game_over():
+    #TODO: (2025-02-12): Add this game over code
+    global game_over_text, is_paused, score, burgers_eaten, player_lives, boost_level, burger_velocity, running
+    if player_lives == 0:
+        game_over_text = font.render(f"FINAL SCORE: {score}", True, ORANGE)
+        display_surface.blit(game_over_text, game_over_rect)
+        display_surface.blit(continue_text, continue_rect)
+        pygame.display.update()
+        pygame.mixer.music.stop()
+        is_paused = True
+        while is_paused:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    score = 0
+                    burgers_eaten = 0
+                    player_lives = PLAYER_STARTING_LIVES
+                    boost_level = STARTING_BOOST_LEVEL
+                    burger_velocity = STARTING_BURGER_VELOCITY
+                    pygame.mixer.music.play()
+                    is_paused = False
+                if event.type == pygame.QUIT:
+                    is_paused = False
+                    running = False
+
+def display_hud():
+    display_surface.fill(BLACK)
+    display_surface.blit(points_text, points_rect)
+    # TODO (2025-02-10): We just blit points_text and points_rect
+    # TODO (cont.):  repeat for score, title, eaten, lives, boost
+    pygame.draw.line(display_surface, WHITE, (0, 100), (WINDOW_WIDTH, 100), 3)
+    # TODO (2025-02-10): blit player_image, player_rect
+    # TODO (2025-02-10): blit burger_image, burger_rect
+    pass  # TODO: (2025-02-10):  remove this when done.
+
+def handle_clock():
+    pygame.display.update()
+    clock.tick(FPS)
+    pass  # TODO: (2025-02-10):  remove this when done.
+
+while running:
+    #TODO: (2025-02-12): Add the function calls below
+    check_quit()
+    move_player()
+    move_burger()
+    handle_miss()
+    check_collisions()
+    update_hud()
+    check_game_over()
+    display_hud()
+    handle_clock()
+
+#TODO: (2025-02-12): Add this call to pygame.quit()
+pygame.quit()
