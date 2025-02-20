a201898e6e6f5362331fa66fcb1a1dd8cbc56d9e
Nicholas Armstrong <narmstro@warren.k12.in.us>
2025-02-20 09:42:37 -0500

    Block 16

--- Unit 04_OOP in Python/Section 01_Classes and Objects/Block 16/assignment_start.py
+++ Unit 04_OOP in Python/Section 01_Classes and Objects/Block 16/assignment_start.py
@@ -9,22 +9,24 @@ class Library:
         self.books = []
 
     def add_book(self, book):
-        #TODO: append to self.books the book.
-        pass #TODO: remove when done
+        #TODO: use self.books's append method and pass in book as the append method's only argument
+        pass  #TODO: remove when done.
 
     def remove_book(self, book):
-        #TODO: remove from self.books the book
-        pass #TODO: remove when done
+        #TODO: use self.books's remove method and pass in book as the remove method's only argument
+        pass  #TODO: remove when done.
 
     def list_books(self):
-        #TODO: make a for loop, for book in self.books
-        #TODO: inside the for loop's block print
-        #TODO: f"{book.title} by {book.author}
-        pass #TODO: remove when done
+        #Note: need a for loop
+        for book in self.books:
+            #TODO: print out f"{book.title} by {book.author}
+            pass  # TODO: remove when done.
+
 
 my_library = Library()
-my_book_1 = Book("To Kill a Mockingbirg", "Harper Lee")
-my_book_2 = Book("Silent Spring", "Rachel Carson")
-my_library.add_book(my_book_1)
-my_library.add_book(my_book_2)
+
+book0 = Book("To Kill a Mockingbird", "Harper Lee")
+book1 = Book("The Great Gatsby", "F. Scott Fitzgerald")
+my_library.add_book(book0)
+my_library.add_book(book1)
 my_library.list_books()
