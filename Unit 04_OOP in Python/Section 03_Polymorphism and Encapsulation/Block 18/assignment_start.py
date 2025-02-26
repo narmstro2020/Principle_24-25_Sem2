3320b143dc88e2ffd101bd5ad1cb4e90dcb00462
Nicholas Armstrong <narmstro@warren.k12.in.us>
2025-02-26 09:33:33 -0500

    Block 18

---
+++ Unit 04_OOP in Python/Section 03_Polymorphism and Encapsulation/Block 18/assignment_start.py
@@ -0,0 +1,29 @@
+class Car:
+    def __init__(self, brand, model, speed, fuel):
+        #TODO: make a brand attribute for self and assign brand to it.
+        #TODO: repeat for model
+        #TODO: make a private speed attribute (hint:  __speed) and assign speed to it
+        #TODO: repeat for fuel
+        pass #TODO: remove when done.
+
+    def get_speed(self):
+        #TODO: return the value of self's speed attribute
+        pass #TODO: remove when done.
+
+
+    def get_fuel(self):
+        #TODO: return the value of self's fuel attribute
+        pass #TODO: remove when done.
+
+    def set_speed(self, speed):
+        #TODO: check if the speed is less than or equal to 200, then set self's speed to speed
+        #TODO: do nothing if speed is greater than 200
+        pass #TODO: remove when done.
+
+    def set_speed(self, fuel):
+        #TODO: same idea as set_speed but in somewhat of a different order
+        pass #TODO: remove when done.
+
+
+#TODO: make a car instance, set its speed, set its fuel.  print out its speed and fuel
+#(cont.): using getting methods.
