f9cd2361d72e103ad42ba2650cb5a7ce0ca75884
Nicholas Armstrong <narmstro@warren.k12.in.us>
2025-03-06 09:33:09 -0500

    Block 20

---
+++ Unit 04_OOP in Python/Section 04_Advanced OOP/Block 20/assignment_start.py
@@ -0,0 +1,56 @@
+# Assignment 1
+from abc import ABC, abstractmethod
+from math import pi
+
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        ...
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        # TODO: make a self.radius and assign radius to it
+        ...  # TODO: remove this  when done
+
+    def area(self):
+        #TODO: return the area of a circle using self.radius
+        #TODO: (cont.):  the area of a circle is pi * (radius ** 2)
+        ...  # TODO: remove this  when done
+
+#TODO: you do the Rectangle in the same way we did a circle.  Except
+#(cont.) instead of a radius you'll need width and length for the init method
+#(cont.) and the area will width * length.  Again don't forget self.
+
+
+#TODO: make a circle and print out its area by calling it.
+#TODO: do the same for a rectangle
+
+# Assignment 2
+
+class BankAccount:
+    def __init__(self, balance, owner):
+        self.balance = balance
+        self.owner = owner
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        self.balance -= amount
+
+    def __str__(self):
+        #TODO: return an f string that prints out the balance of the account
+        #TODO: (cont.) format of the string: "{owner}'s Account: Balance of {balance}
+        ... #TODO: remove this .. when done
+
+    def __add__(self, other):
+        #TODO: return the sum of self.balance and other.balance
+        ... #TODO: remove this .. when done
+
+fred = BankAccount(500, "Fred")
+ethel = BankAccount(600, "Ethel")
+print(fred)
+print(ethel)
+print(fred + ethel)
