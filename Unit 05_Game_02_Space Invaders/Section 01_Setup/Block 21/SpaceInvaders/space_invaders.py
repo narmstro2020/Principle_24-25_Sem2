a5199d29c0e3f9b2a4f5cbc9b4feb72fced341c5
Nicholas Armstrong <narmstro@warren.k12.in.us>
2025-03-07 07:53:13 -0500

    Block 21

--- Unit 05_Game_02_Space Invaders/Section 01_Setup/Block 21/SpaceInvaders/space_invaders.py
+++ Unit 05_Game_02_Space Invaders/Section 01_Setup/Block 21/SpaceInvaders/space_invaders.py
@@ -18,4 +18,18 @@ class Game:
     """A class to help control and update gameplay"""
 
     def __init__(self, player, alien_group, player_bullet_group, alien_bullet_group):
-        pass
+        """Initialize the game"""
+        #Set game values
+        self.round_number = 1
+        self.score = 0
+        # TODO: set the player to self.player
+        # TODO: repeat for alien_group, player_bullet_group, alien_bullet_group.  set all to a self. version.
+
+        #Set sounds and music
+        self.new_round_sound = pygame.mixer.Sound("./assets/audio/new_round.wav")
+        #TODO: repeat for breach_sound (using self) breach.wav
+        #TODO: (cont.) alien_hit_sound to alien_hit.wav
+        #TODO: (cont.) player_hit_sound to player_hit.wav
+
+        #Set font
+        self.font = pygame.font.Font("Facon.ttf", 32)
