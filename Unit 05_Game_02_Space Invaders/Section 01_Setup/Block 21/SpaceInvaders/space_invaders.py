83d5e75ec59cd4562e6786c455422faf2484b0b8
Nicholas Armstrong <narmstro@warren.k12.in.us>
2025-03-07 07:42:01 -0500

    Block 21

---
+++ Unit 05_Game_02_Space Invaders/Section 01_Setup/Block 21/SpaceInvaders/space_invaders.py
@@ -0,0 +1,10 @@
+import pygame, random
+
+#Intialize pygame
+pygame.init()
+
+#Set Display Surface
+WINDOW_WIDTH = 1200
+WINDOW_HEIGHT = 700
+size = (WINDOW_WIDTH, WINDOW_HEIGHT)
+display_surface = pygame.display.set_mode(size)
